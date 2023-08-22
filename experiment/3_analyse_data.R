22f38d7370d645211bfca7fa03fb090d95e6ef8b
Stefanie Meliss, PhD <s_meliss@outlook.com>
2023-08-22 09:44:16 +0100

    Delete 3_analyse_data.R

--- experiment/3_analyse_data.R
+++
@@ -1,5 +0,0 @@
-# compute cor matrix for fo subscales
-psych::corr.test(data[, grepl("fo_", names(data))])
-
-# compute descriptives for fo subscales
-psych::describe(data[, grepl("fo_", names(data))])
